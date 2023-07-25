f7735903469d78dc7550b421802b37c796b06c4b
anderson cespedes <andersontrabajos2@gmail.com>
2023-07-25 11:45:04 -0500

    auxilio

--- Program.cs
+++ Program.cs
@@ -1,16 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-/*Crea un programa en C# que permita a los usuarios administrar una libreta telefónica. El programa debería tener un menú con las siguientes opciones:
-    Agregar entrada
-    Mostrar todas las entradas
-    Marcar entrada como importante
-    Eliminar entrada 
-    Salir
-    */
 class Contacto {
     public int? Numero {get; set;}
     public string? Nombre {get; set;}
-    public bool? Favorito {get; set;}
+    public bool Favorito {get; set;}
     public Contacto(int? numero, string? nombre){
         Numero = numero;
         Nombre = nombre;
@@ -25,39 +18,52 @@ class Program {
         int opcion;
 
         do {
+            Console.Clear();
             MostrarMenu();
             opcion = PedirOpcion();
 
             switch (opcion) {
                 case 1:
-                    Saludar();
-                    break;
-                case 2:
+                    Console.Clear();
                     AgregarEntrada();
                     break;
-                case 3:
+                case 2:
+                    Console.Clear();
                     MostrarEntradas();
+                    Console.Write("presione enter para continuar ->");
+                    Console.ReadLine();
+                    break;
+                case 3:
+                    Console.Clear();
+                    MarcaFavorito();
                     break;
                 case 4:
-                    Console.WriteLine("Hasta luego.");
+                    Console.Clear();
+                    EliminarEntrada();
+                    break;
+                case 5:
+                    Console.WriteLine("hasta luego :)");
                     break;
                 default:
                     Console.WriteLine("Opción inválida. Inténtalo de nuevo.");
                     break;
             }
-
-            Console.WriteLine();
-        } while (opcion != 4);
+        } while (opcion != 5);
     }
 
     static void MostrarMenu() {
         Console.WriteLine("Menú de opciones:");
-        Console.WriteLine("1. Saludar");
-        Console.WriteLine("2. Agregar Entrada");
-        Console.WriteLine("3. Mostrar Entradas");
-        Console.WriteLine("4. Salir");
+        Console.WriteLine("1. Agregar Entrada");
+        Console.WriteLine("2. Mostrar Entradas");
+        Console.WriteLine("3. Marcar Entrada Como Favorito");
+        Console.WriteLine("4. Eliminar Entrada");
+        Console.WriteLine("5. Salir");
     }
-
+    static void EliminarEntrada(){
+        MostrarEntradas();
+        int id = PedirOpcion();
+        contact.Remove(id);
+    }
     static int PedirOpcion() {
         Console.Write("Elige una opción: ");
         return Convert.ToInt32(Console.ReadLine());
@@ -68,24 +74,36 @@ class Program {
     }
 
     static void AgregarEntrada() {
-        Console.Write("Ingresa un número: ");
-        int numero = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Ingrese un Nombre");
-        string nombre = Console.ReadLine();
-        Contacto cont = new Contacto(numero, nombre);
-        Dictionary<int, Contacto>.KeyCollection keys = contact.Keys;
-        contact.Add(keys.Last() + 1, cont);
+        try{
+            Console.Write("Ingresa un número: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese un Nombre: ");
+            string nombre = Console.ReadLine();
+            Contacto cont = new Contacto(numero, nombre);
+            Dictionary<int, Contacto>.KeyCollection keys = contact.Keys;
+            contact.Add(keys.Last() + 1, cont);
+        }catch(FormatException e){
+            Console.WriteLine(e.Message);
+        }
+        
     }
-
+    static void MarcaFavorito(){
+        MostrarEntradas();
+        int opcion = PedirOpcion();
+        contact[opcion].Favorito = true;
+        Console.WriteLine("Se ha agregado un favorito");
+    }
     static void MostrarEntradas() {
+        Console.WriteLine("Id  Nombre  Telefono  Favorito");
+        Console.WriteLine("------------------------------");
         foreach (KeyValuePair<int, Contacto> pareja in contact)
         {
             int clave = pareja.Key;
             Contacto valor = pareja.Value;
             if(clave > 0){
-                Console.WriteLine($"Clave: {clave}, Valor: {valor.Nombre}");
+                string fav = valor.Favorito ? "SI" : "NO";
+                Console.WriteLine($"{clave} | {valor.Nombre} | {valor.Numero} | {fav}");
             }
-            
         }
     }
 }
